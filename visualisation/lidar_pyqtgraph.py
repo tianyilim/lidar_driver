9c4f599f9b16f58b8454945efe5dc81ba5f46808
Jia Qi Poon <poonjiaqi@gmail.com>
2020-09-20 19:04:15 +0800

    Add multithreading to plotting of graph, UNSTABLE

--- visualisation/lidar_pyqtgraph.py
+++ visualisation/lidar_pyqtgraph.py
@@ -1,39 +1,61 @@
 from pyqtgraph.Qt import QtGui, QtCore
+from PyQt5.QtCore import QRunnable, QThreadPool, pyqtSlot
 import pyqtgraph as pg
 import numpy as np
 import time
+import serial
+import serial.tools.list_ports
 
 np.random.seed(42)
 
+class Worker(QRunnable):
+    def __init__(self, main_class):
+        super(Worker, self).__init__()
+        self.ser = main_class.ser
+        self.x_data = main_class.x_data
+        self.y_data = main_class.y_data
+        self.s1 = main_class.s1
+
+    # @pyqtSlot()
+    def run(self):
+        while True:
+            # num = int(self.ser.readline())
+            num1 = np.random.random()
+            num2 = np.random.random()
+
+            self.x_data.append(num1)
+            self.y_data.append(num2)
+
+            self.s1.setData(x=self.x_data, y=self.y_data)
+            time.sleep(0.01)
+
 class mainApp():
     def __init__(self):
+        # Setup windows
         app = QtGui.QApplication([])
         mw = QtGui.QMainWindow()
         mw.resize(800,800)
-        view = pg.GraphicsLayoutWidget()  ## GraphicsView with GraphicsLayout inserted by default
+        view = pg.GraphicsLayoutWidget() 
         mw.setCentralWidget(view)
-
         w1 = view.addPlot()
-
-        np.random.seed(42)
-        n = 10
         s1 = pg.ScatterPlotItem(size=10, pen=pg.mkPen(None), brush=pg.mkBrush(255, 255, 255, 200))
         w1.addItem(s1)
+
+        # Setup serial
+        s = [port.device for port in serial.tools.list_ports.comports()]
+        self.ser = serial.Serial(s[1])
 
         self.s1 = s1
         self.mw = mw
         self.x_data=[]
         self.y_data=[]
 
-    def addPoint(self):
-        self.x_data.append(np.random.random())
-        self.y_data.append(np.random.random())
-        self.s1.setData(x=self.x_data, y=self.y_data)
+        # Start worker thread to plot data
+        self.threadpool = QThreadPool()
+        worker = Worker(self)
+        self.threadpool.start(worker) 
 
     def show(self):
-        timer = QtCore.QTimer()
-        timer.timeout.connect(self.addPoint)
-        timer.start(10) # Timeout in milliseconds
         self.mw.show()
         QtGui.QApplication.instance().exec_()
 
